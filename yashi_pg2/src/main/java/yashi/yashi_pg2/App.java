a4ad240587aedbf59628c0c235cc16948057d91a
Yashika Pal <yashibang2004@gmail.com>
2025-06-01 23:00:54 +0530

    Add files via upload

---
+++ yashi_pg2/src/main/java/yashi/yashi_pg2/App.java
@@ -0,0 +1,13 @@
+package yashi.yashi_pg2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
